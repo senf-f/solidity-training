ae53040f5d28fae1fdf4743573a9fc43c31554e5
senf-f <mate.mrse@gmail.com>
2025-05-16 10:16:16 +0000

    first commit

---
+++ firstDapp.sol
@@ -0,0 +1,14 @@
+// import solidity
+pragma solidity >=0.8.2 <0.9.0;
+
+//import
+import "filename";
+import * as symbolname from "filename2";
+import {symbol1 as alies, symbol2} from "filename3";
+
+// your first contract
+contract SimpleContract {
+    string name = "Thomas";
+    uint storedata = 34;
+    bool a = false;
+}
