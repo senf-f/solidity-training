82ec13d44cfe8ec0fad8f13817ba38035f0fd63f
senf-f <mate.mrse@gmail.com>
2025-05-16 10:25:16 +0000

    practice

---
+++ anotherDapp.sol
@@ -0,0 +1,14 @@
+// import solidity
+pragma solidity >=0.8.2 <0.9.0;
+
+contract SimpleContract {
+
+    // currency: ether gwei wei
+    bool isEqual = (1 ether == 1e18 wei);
+
+    // time units
+    bool isTime = (1 hours == 60 minutes);
+
+    
+
+}
