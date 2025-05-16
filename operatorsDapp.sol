fdf240acb7ee4c623f5cfa7f79ef6d955c8c22b9
senf-f <mate.mrse@gmail.com>
2025-05-16 14:17:57 +0000

    on and on

---
+++ operatorsDapp.sol
@@ -0,0 +1,16 @@
+// import solidity
+pragma solidity >=0.8.2 <0.9.0;
+
+contract SimpleContract {
+
+    uint a = 2;
+    uint b = 3;
+    uint c = a + b;
+
+    // logical: ! && || == !=
+
+    // Bitwise
+    bytes1 contractValue = 0x02 | 0x01;
+
+
+}
